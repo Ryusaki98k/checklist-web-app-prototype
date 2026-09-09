55e0cfdb52599db5a56b6f75b37eca56bed66f50
Jittipon Pannak <jittiponpannak@gmail.com>
2026-09-09 17:12:16 +0700

    database usable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import { users } from './db/schema'
+import { eq } from 'drizzle-orm';
+import 'dotenv/config';
 
-async function main() {
-    const client = postgres(process.env.DATABASE_URL!)
-    const db = drizzle({ client });
+const queryClient = postgres(process.env.DATABASE_URL!, { prepare: false });
+export const db = drizzle({ client: queryClient });
+
+export async function getUserById(id: number) {
+    const result = await db.select().from(users).where(eq(users.id, id));
+    return result[0];
 }
-
-main();
