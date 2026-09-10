508d2c104fd9b96237c70d4ba925fba61a28c474
Jittipon Pannak <jittiponpannak@gmail.com>
2026-09-10 15:55:57 +0700

    schema update
    after meeting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import { users } from './db/schema'
-import { eq } from 'drizzle-orm';
 import 'dotenv/config';
+
+enum TaskStatus { InCompleted, Completed, Late }
 
 const queryClient = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle({ client: queryClient });
-
-export async function getUserById(id: number) {
-    const result = await db.select().from(users).where(eq(users.id, id));
-    return result[0];
-}
